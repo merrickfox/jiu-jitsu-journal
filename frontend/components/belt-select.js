79ed2b77af99a3fed0beb6d6b4b90af0ecb787b1
Merrick Fox <merrick.fox@gmail.com>
2018-06-03 15:07:16 +0100

    belt graphic

--- frontend/components/belt-select.js
+++ frontend/components/belt-select.js
@@ -10,8 +10,6 @@ class BeltSelect extends React.Component {
 	};
 
 	handleSelectChange = name => event =>  {
-		console.log('select change', name)
-		console.log('select change', event)
 		this.setState({
 			[name]: event.value,
 		});
@@ -32,23 +30,23 @@ class BeltSelect extends React.Component {
 									options={[
 										{
 											value: 'White',
-											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>White</span>  <span className='secondary'>  <div className='belt white'><div className="strap"></div> </div>   </span></Box>
+											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>White</span>  <span className='secondary'>  <div className='belt white'><div className="strap"></div> <div className="texture top"></div><div className="texture middle"></div><div className="texture middle-2"></div><div className="texture middle-3"></div><div className="texture bottom"></div> </div>   </span></Box>
 										},
 										{
 											value: 'Blue',
-											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Blue</span>  <span className='secondary'>  <div className='belt blue'><div className="strap"></div> </div>   </span></Box>
+											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Blue</span>  <span className='secondary'>  <div className='belt blue'><div className="strap"></div><div className="texture top"></div><div className="texture middle"></div><div className="texture middle-2"></div><div className="texture middle-3"></div><div className="texture bottom"></div> </div>   </span></Box>
 										},
 										{
 											value: 'Purple',
-											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Purple</span>  <span className='secondary'>  <div className='belt purple'><div className="strap"></div> </div>   </span></Box>
+											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Purple</span>  <span className='secondary'>  <div className='belt purple'><div className="strap"></div><div className="texture top"></div><div className="texture middle"></div><div className="texture middle-2"></div><div className="texture middle-3"></div><div className="texture bottom"></div> </div>   </span></Box>
 										},
 										{
 											value: 'Brown',
-											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Brown</span>  <span className='secondary'>  <div className='belt brown'><div className="strap"></div> </div>   </span></Box>
+											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Brown</span>  <span className='secondary'>  <div className='belt brown'><div className="strap"></div><div className="texture top"></div><div className="texture middle"></div><div className="texture middle-2"></div><div className="texture middle-3"></div><div className="texture bottom"></div> </div>   </span></Box>
 										},
 										{
 											value: 'Black',
-											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Black</span>  <span className='secondary'>  <div className='belt black'><div className="strap red"></div> </div>   </span></Box>
+											label: <Box direction='row'  justify='between' className='select-value'>  <span title='White'>Black</span>  <span className='secondary'>  <div className='belt black'><div className="strap red"></div><div className="texture top"></div><div className="texture middle"></div><div className="texture middle-2"></div><div className="texture middle-3"></div><div className="texture bottom"></div> </div>   </span></Box>
 										},
 									]}
 									value={this.state.belt}
@@ -59,7 +57,7 @@ class BeltSelect extends React.Component {
           .belt {
             border: 1px solid black;
             width: 100px;
-            height: 100%;
+            height: 24px;
             position: relative;
           }
 
@@ -69,7 +67,7 @@ class BeltSelect extends React.Component {
             height: 100%;
             background-color: black;
             position: absolute;
-
+            z-index: 10;
           }
 
           .white {
@@ -77,24 +75,24 @@ class BeltSelect extends React.Component {
           }
 
           .blue {
-            background-color: #2c4fff;
+            background: linear-gradient(90deg, #2c4fff, #3393ff);
           }
 
           .purple {
-            background-color: rebeccapurple;
+            background: linear-gradient(90deg, rebeccapurple, #7b6399);
           }
 
           .brown {
-            background-color: saddlebrown;
+            background: linear-gradient(90deg, saddlebrown, #8b6248);
           }
 
           .black {
             background-color: black;
           }
 
-					.red {
+          .red {
             background-color: red;
-					}
+          }
 
           .form-field {
             min-width: 330px;
@@ -103,6 +101,38 @@ class BeltSelect extends React.Component {
           .grommetux-select__options {
             padding: 0 1em;
             background-color: white;
+          }
+
+          .select-value {
+            padding: 0 15px;
+            align-items: center;
+          }
+
+          .texture {
+            width: 100%;
+            height: 2px;
+            position: absolute;
+            box-shadow: inset 5px 6px 5px -7px rgba(0, 0, 0, 0.75);
+          }
+
+          .bottom {
+            top: 2px;
+          }
+
+          .middle {
+            top: 6px;
+          }
+
+          .middle-2 {
+            top: 10px;
+          }
+
+          .middle-3 {
+            top: 14px;
+          }
+
+          .top {
+            top: 18px;
           }
 
 				`}</style>
